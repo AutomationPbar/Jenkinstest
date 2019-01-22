604130d88e97426d9e8d360b06d62be88b601ff2
Avani Bansal <Avani@Etechaces.com>
2019-01-22 15:37:16 +0530

    added new file firsttest

---
+++ src/test/FirstTest.java
@@ -0,0 +1,12 @@
+package test;
+
+public class FirstTest {
+
+	public static void main(String[] args) {
+		
+		System.out.println("testing first time");
+		// TODO Auto-generated method stub
+
+	}
+
+}
